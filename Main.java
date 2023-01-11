b4d9822a956842a44c590589e99cd79c6f592df3
Janko <45310863+WatchJani@users.noreply.github.com>
2023-01-11 22:21:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,36 +16,92 @@ public class Main {
         //New.setLayout(new FlowLayout(FlowLayout.CENTER));
         SETTING.setPreferredSize(new Dimension(500, 530));
         //buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        
-        SETTING.setBackground(Color.gray);
+
+        //SETTING.setBackground(Color.gray);
         Container.setBorder(new EmptyBorder(60, 0, 0, 0)); //EmptyBorder(int top, int left,
 
 
         //dificulty
         JPanel DIFFICULTY = new JPanel();
-        DIFFICULTY.setPreferredSize(new Dimension(500, 100));
-        DIFFICULTY.setBackground(Color.YELLOW);
+        DIFFICULTY.setLayout(new BoxLayout(DIFFICULTY, BoxLayout.Y_AXIS));
+        DIFFICULTY.setPreferredSize(new Dimension(500, 80));
+        //DIFFICULTY.setBackground(Color.YELLOW);
+        JLabel DIFFICULTY_LABEL = new JLabel("Choose a difficulty!");
+        DIFFICULTY.add(DIFFICULTY_LABEL);
 
+        JPanel PANEL_BUTTON = new JPanel();
+
+        JButton EASY = new JButton("EASY");
+        JButton NORMAL = new JButton("NORMAL");
+        JButton HARD = new JButton("HARD");
+
+        PANEL_BUTTON.add(EASY);
+        PANEL_BUTTON.add(NORMAL);
+        PANEL_BUTTON.add(HARD);
+
+        PANEL_BUTTON.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        DIFFICULTY.add(PANEL_BUTTON);
+
+        //===========================================================================
 
         //size of the playing field
         JPanel PLAYER_FIELD = new JPanel();
-        PLAYER_FIELD.setPreferredSize(new Dimension(500, 100));
-        PLAYER_FIELD.setBackground(Color.RED);
+        PLAYER_FIELD.setLayout(new BoxLayout(PLAYER_FIELD, BoxLayout.Y_AXIS));
+        PLAYER_FIELD.setPreferredSize(new Dimension(500, 50));
+        //PLAYER_FIELD.setBackground(Color.RED);
+        JLabel SIZE_OF_THE_PLAYING_FIELD_LABEL = new JLabel("Choose a SIZE OF THE PLAYING FIELD");
+        PLAYER_FIELD.add(SIZE_OF_THE_PLAYING_FIELD_LABEL);
+
+        JLabel COLUMN_LABEL = new JLabel("Column:");
+        JLabel ROW_LABEL = new JLabel("Row:");
+
+        JPanel FIELD_PANEL = new JPanel();
+
+        FIELD_PANEL.add(COLUMN_LABEL);
+        JTextField ROW_FIELD = new JTextField(8);
+        FIELD_PANEL.add(ROW_FIELD);
+
+        FIELD_PANEL.add(ROW_LABEL);
+        JTextField COLUMN = new JTextField(8);
+        FIELD_PANEL.add(COLUMN);
+
+        FIELD_PANEL.setAlignmentX(Component.LEFT_ALIGNMENT);
+        PLAYER_FIELD.add(FIELD_PANEL);
+
+        //===========================================================================
 
         //number of moves
         JPanel NUMBER_OF_MOVES = new JPanel();
         NUMBER_OF_MOVES.setPreferredSize(new Dimension(500, 100));
         NUMBER_OF_MOVES.setBackground(Color.BLUE);
+        JLabel NUMBER_OF_MOVES_LABEL = new JLabel("Choose a NUMBER OG MOVES");
+        NUMBER_OF_MOVES.add(NUMBER_OF_MOVES_LABEL);
+
+
+
+
+        //===========================================================================
+
 
         //target value setting
         JPanel TARGET_VALUE_SETTING = new JPanel();
         TARGET_VALUE_SETTING.setPreferredSize(new Dimension(500, 100));
         TARGET_VALUE_SETTING.setBackground(Color.PINK);
+        JLabel TARGET_VALUE_SETTING_LABEL = new JLabel("Choose a TARGET VALUE SETTING");
+        TARGET_VALUE_SETTING.add(TARGET_VALUE_SETTING_LABEL);
+
+        //===========================================================================
+
 
         //target value setting
         JPanel BUTTONS = new JPanel();
         BUTTONS.setPreferredSize(new Dimension(500, 100));
         BUTTONS.setBackground(Color.GREEN);
+        JButton BACK = new JButton("BACK");
+        JButton SAVE = new JButton("SAVE");
+        BUTTONS.add(BACK);
+        BUTTONS.add(SAVE);
 
 
         Container.add(SETTING);
